c173d73efffa11ef6e11ebcfee22a4d68d5c2c30
ivan ahumada <57021827+ivan-ahumada@users.noreply.github.com>
2021-10-31 10:46:27 -0500

    Create SoundStreamFunction.py

---
+++ scripts/SoundStreamFunction.py
@@ -0,0 +1,22 @@
+import random
+
+def sound_stream(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    
+    x = random.randint(1,5)
+    y = str(x)
+
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return y
